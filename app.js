b3286aead3efee7adac9b29c3cde5e8d96d527e2
RapRy <57054204+RapRy@users.noreply.github.com>
2021-04-27 08:09:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express()
 const dbURI = "mongodb://testUser:test123@cluster0-shard-00-00.vu7xx.mongodb.net:27017,cluster0-shard-00-01.vu7xx.mongodb.net:27017,cluster0-shard-00-02.vu7xx.mongodb.net:27017/TestingDB?ssl=true&replicaSet=atlas-5qniev-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((res) => app.listen(process.env.PORT, () => console.log('server listening at port 3000')))
     .catch(err => console.log(err))
 
 app.set('view engine', 'ejs')
@@ -20,6 +21,3 @@ app.use(express.urlencoded({ extended:true }))
 
 // routes
 app.use('/', todoRoutes)
-
-
-app.listen(process.env.PORT, () => console.log('server listening at port 3000'))
